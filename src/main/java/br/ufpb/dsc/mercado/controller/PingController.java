8dbc2f5b4882549886536b130a833b4e7658ab3c
Jau Italo <jau.batista@dcx.ufpb.br>
2026-06-03 17:48:53 -0300

    implementado o ping

---
+++ src/main/java/br/ufpb/dsc/mercado/controller/PingController.java
@@ -0,0 +1,17 @@
+package br.ufpb.dsc.mercado.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+  @GetMapping("/ping")
+  public Map<String,Object> ping() {
+    return Map.of(
+      "status", "ok",
+      "service", "eq05",
+      "timestamp", java.time.Instant.now().toString());
+  }
+}
