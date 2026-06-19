7f3f36eed24387f65663b43017a2514e25a6308b
Jau Italo <jau.batista@dcx.ufpb.br>
2026-06-19 00:01:37 -0300

    fix: adiciona método buscarMaisVendidos ao ProdutoService

--- backend/src/main/java/br/ufpb/dsc/mercado/repository/ProdutoRepository.java
+++ backend/src/main/java/br/ufpb/dsc/mercado/repository/ProdutoRepository.java
@@ -1,10 +1,11 @@
 package br.ufpb.dsc.mercado.repository;
 
-import br.ufpb.dsc.mercado.domain.Produto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import br.ufpb.dsc.mercado.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
@@ -18,4 +19,6 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     Page<Produto> findByCategoriaIdAndAtivoTrue(Long categoriaId, Pageable pageable);
 
     Page<Produto> findByCategoriaId(Long categoriaId, Pageable pageable);
+
+    public Page<Produto> findMaisVendidos(Pageable pageable);
 }
