ac6edd12624527651beaa3882514411dba98edec
UpSnow <arthurcel2002@gmail.com>
2026-07-21 20:14:23 -0300

    fix(docker): expor nginx na porta 8105 atribuída pelo servidor

--- target/classes/db/migration/V3__adicionar_campos_usuario.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE usuario
-    ADD COLUMN IF NOT EXISTS sobrenome        VARCHAR(100),
-    ADD COLUMN IF NOT EXISTS cpf              VARCHAR(14) UNIQUE,
-    ADD COLUMN IF NOT EXISTS telefone         VARCHAR(15),
-    ADD COLUMN IF NOT EXISTS data_nascimento  DATE;
